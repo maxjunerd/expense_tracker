4b2476ad078f1288eb63dafd4e7ca55d248b32d3
Tonsai <chayanon.tonsai@adastragrp.com>
2021-12-06 03:44:42 +0700

    Add form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 import './App.css'
 
 function App() {
@@ -26,7 +27,7 @@ function App() {
 
   return (
     <div className="App">
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses expenses={expenses}/>
     </div>
   );
